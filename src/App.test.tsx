9a0c74481421043138aa89494cbdd8a11ef82592
sakata1222 <sakata1222@gmail.com>
2025-03-29 08:37:06 +0900

    fix: do not use removed apis

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const root = ReactDOM.createRoot(div);
+  root.render(<App />);
+  root.unmount();
 });
