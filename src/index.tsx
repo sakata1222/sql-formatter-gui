4e1a0bd7087caee8dbbefcf4ba0f3c847400ba97
sakata1222 <sakata1222@gmail.com>
2026-09-22 22:46:33 +0900

    refactor: remove unused BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -9,9 +8,7 @@ const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
 
